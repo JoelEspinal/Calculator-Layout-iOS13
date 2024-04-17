63a723fd14d0620be82058cd443206aef45276be
JoelEspinal <joel_espinal@claro.com.do>
2024-04-17 14:47:40 -0400

    Add key button type

--- Calculator Layout iOS13/ViewController.swift
+++ Calculator Layout iOS13/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        if let numValue = sender.currentTitle, var displayText = displayLabel.text {
+            if displayText != "0" {
+                displayText = displayText + "\(numValue)"
+            } else {
+                displayText = numValue
+            }
+            
+            displayLabel.text = displayText
+        }
+    }
+    
 }
 
